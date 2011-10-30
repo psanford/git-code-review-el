90194458a200a777b72776f6a3aad895671c3f06
Peter Sanford <peter@petersdanceparty.com>
2011-10-29 20:34:33 -0700

    Added ability to mark and unmark commits.
    This will be used to mark multiple commits as reviewed.

--- git-code-review.el
+++ git-code-review.el
@@ -68,6 +68,14 @@
   (interactive)
   (magit-show-commit (git-code-review-current-commit)))
 
+(defun git-code-review-current-commit-region ()
+  "Returns a list of point min and point max for current commit"
+  (list
+   (line-beginning-position)
+   (save-excursion
+     (forward-line 1)
+     (line-beginning-position))))
+
 (defvar git-code-review-highlight-overlay nil)
 (defun git-code-review-highlight-line ()
   "Highlight current line"
@@ -75,11 +83,8 @@
       (let ((ov (make-overlay 1 1)))
         (overlay-put ov 'face 'magit-item-highlight)
         (setq git-code-review-highlight-overlay ov)))
-  (move-overlay git-code-review-highlight-overlay
-                (line-beginning-position)
-                (save-excursion
-                  (forward-line 1)
-                  (line-beginning-position))))
+  (apply 'move-overlay git-code-review-highlight-overlay
+         (git-code-review-current-commit-region))0)
 
 (defun git-code-review-post-command-hook ()
   (git-code-review-highlight-line))
@@ -109,11 +114,45 @@
       (git-code-review-add-value "skip" commit)
       (git-code-review))))
 
+(defvar git-code-review-marked-commits '())
+(make-variable-buffer-local 'git-code-review-marked-commits)
+
+(defun git-code-review-unmark-commit (line-move-number)
+  "Unmark current commit"
+  (interactive "p")
+  (let ((commit (git-code-review-current-commit)))
+    (setq git-code-review-marked-commits
+          (remove commit git-code-review-marked-commits))
+    (apply 'remove-overlays
+           (append
+            (git-code-review-current-commit-region)
+            '(git-code-review-type mark))))
+  (forward-line (or line-move-number 1)))
+
+(defun git-code-review-mark-commit (line-move-number)
+  "Mark current commit"
+  (interactive "p")
+  (let ((commit (git-code-review-current-commit))
+        (overlay))
+      (add-to-list 'git-code-review-marked-commits commit)
+      (setq overlay (apply 'make-overlay (git-code-review-current-commit-region)))
+      (overlay-put overlay 'face 'magit-item-mark)
+      (overlay-put overlay 'git-code-review-type 'mark))
+  (forward-line (or line-move-number 1)))
+
 (defvar git-code-review-mode-map
   (let ((map (make-keymap)))
     (define-key map (kbd "RET") 'git-code-review-view-diff)
     (define-key map (kbd "g") 'git-code-review-view-in-github)
     (define-key map (kbd "r") 'git-code-review-mark-commit-as-reviewed)
+    (define-key map (kbd "m") 'git-code-review-mark-commit)
+    (define-key map (kbd "M") '(lambda ()
+                                 (interactive)
+                                 (git-code-review-mark-commit -1)))
+    (define-key map (kbd "u") 'git-code-review-unmark-commit)
+    (define-key map (kbd "U") '(lambda ()
+                                 (interactive)
+                                 (git-code-review-unmark-commit -1)))
     map))
 
 (define-derived-mode git-code-review-mode fundamental-mode "Git Code Review"
